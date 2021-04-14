ab8aee83868fa24ce50eb1e4de13a0e891c9f13b
rogerluo <rogerluo@umld.edu>
2021-04-15 04:44:58 +0800

    added login html, created database to store username and password, hashed password for security

--- frontend/forms.py
+++ frontend/forms.py
@@ -1,6 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 
+class LoginForm(FlaskForm):
+    username = StringField('Username')
+    password = PasswordField('Password')
+    submit = SubmitField('Log in')
 
 class SignUpForm(FlaskForm):
     username = StringField('Username')
