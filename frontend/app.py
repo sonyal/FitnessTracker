9874d5fc5ea38a036e34c08f92aa1ce3e47974cf
rogerluo <rogerluo@umld.edu>
2021-04-23 08:53:24 +0800

    Roger's update

--- frontend/app.py
+++ frontend/app.py
@@ -1,9 +1,10 @@
 
 from flask import Flask, redirect, url_for, render_template,flash, request
 from tutorialsearch import RegistrationForm, TestLinkProxy
-from forms import SignUpForm, WorkOutForm, LoginForm
+from forms import SignUpForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user, LoginManager, UserMixin
 from os import path
 
 app = Flask(__name__)
@@ -11,14 +12,24 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fitness_database.db'
 db = SQLAlchemy(app)
 
-class User(db.Model):
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(50), unique = True)
     password = db.Column(db.String(50))
+    weight = db.Column(db.String(30))
+    height = db.Column(db.String(30))
     
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html', user=current_user)
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -30,12 +41,35 @@ def login():
         user = User.query.filter_by(username=username).first()
         if user:
             if check_password_hash(user.password, password):
-                print("Login successfully!")
+                login_user(user, remember=False)
+                return redirect(url_for('user_page'))
             else:
-                print("failed!")
+                flash('Your Login Information is incorrect, please try again!', category='error')
+        else:
+            flash('Your Login Information is incorrect, please try again!', category='error')
 
-    return render_template('login.html', form=form)
+    return render_template('login.html', form=form, user=current_user)
 
+@app.route('/logout')
+@login_required
+def log_out():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route("/user", methods=['GET', 'POST'])
+@login_required
+def user_page():
+    if request.method == 'POST':
+        if request.form.get('new_weight'):
+            new_weight = request.form.get('new_weight')
+            current_user.weight = new_weight
+            db.session.commit()
+        else:
+            new_height = request.form.get('new_height')
+            current_user.height = new_height
+            db.session.commit()
+    
+    return render_template('user_page.html', user=current_user)
 
 @app.route("/Cardiovascular", methods=['GET', 'POST'])
 def cardiovascular():
@@ -72,33 +106,42 @@ def tutorial():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if not path.exists('frontend/fitness_database.db'):
+        db.create_all(app=app)
+
     form = SignUpForm()
+
     if form.is_submitted():
         result = request.form
         username = result.get('username')
-        password = result.get('password')
+        password1 = result.get('password')
+        password2 = result.get('confirm_password')
+        weight = result.get('weight')
+        height = result.get('height')
+    
+        if not confirm_password(password1, password2):
+            flash('Two passwords are not matched', category='error')
+        elif len(username) < 4:
+            flash('Username must be at least 4 characters', category='error')
+        elif len(password1)< 12:
+            flash('Password must be at least 12 characters', category='error')
+        else:
+            try:
+                new_user = User(username=username, password=generate_password_hash(password1, method='sha256'), weight=weight, height=height)
+                db.session.add(new_user)
+                db.session.commit()
+                return redirect(url_for('login'))
+            except:
+                flash('User Creation Failed, please try another username!', category='error')
 
-        try:
-            new_user = User(username=username, password=generate_password_hash(password, method='sha256'))
-            db.session.add(new_user)
-            db.session.commit()
-        except:
-            print("User existed!")
-            return render_template('signup.html', form=form)
-
-        print("User:" + username + " with password:" + password + " is created!")
-
-        return redirect(url_for('login'))
-
-    return render_template('signup.html', form=form)
+    return render_template('signup.html', form=form, user=current_user)
 
 
-@app.route('/WorkOut', methods=['GET', 'POST'])
-def workout():
-    form = WorkOutForm()
-    if form.is_submitted():
-        result = request.form
-    return render_template('WorkOut.html', form=form)
+def confirm_password(p1, p2):
+    if p1 == p2:
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     app.run(debug=True)
