b559c4d45146ed96ddeb6e6011d7ef1002eed0d1
Yifan <qyf1998@terpmail.umd.edu>
2021-05-11 12:53:18 -0400

    fixed database test

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,22 +1,31 @@
-from app import db
+from app import db, app
 from models import User, Plan
-import unittestc
+import unittest
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy import create_engine
 
 class TestDatabase(unittest.TestCase):
-    def test_add_user():
-        new_user = User(username="bob", password=generate_password_hash("testpassword123456", method='sha256'),
-                                weight="70", height"180")
-        db.session.add(new_user)
-        db.session.commit()
 
-        comfirm_user = db.query.filter_by(username="bob").first()
+    def test_add_user(self):
+        db.init_app(app)
+
+        with app.app_context():
+            new_user = User(username="bob", password=generate_password_hash("testpassword123456", method='sha256'),
+                                weight="70", height="180")
+            db.session.add(new_user)
+            db.session.commit()
+
+            comfirm_user = User.query.filter_by(username="bob").first()
         self.assertEqual(new_user, comfirm_user)
     
-    def test_delete_user():
-        user = db.query.filter_by(username="bob").first()
+    def test_delete_user(self):
+        db.init_app(app)
 
-        db.session.delete(user)
-        db.session.commit()
+        with app.app_context():
+            user = User.query.filter_by(username="bob").first()
 
-        comfirm_user = db.query.filter_by(username="bob").first()
+            db.session.delete(user)
+            db.session.commit()
+
+            comfirm_user = User.query.filter_by(username="bob").first()
         self.assertEqual(comfirm_user, None)
