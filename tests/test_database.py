6fe5b432fb4cb0adfbc80452b01e760724cb938c
Yifan <qyf1998@terpmail.umd.edu>
2021-05-11 13:02:17 -0400

    fix database test 2

--- tests/test_database.py
+++ tests/test_database.py
@@ -3,13 +3,21 @@ from models import User, Plan
 import unittest
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy import create_engine
+from os import path
+
 
 class TestDatabase(unittest.TestCase):
 
     def test_add_user(self):
         db.init_app(app)
+        if not path.exists('fitness_database.db'):
+            db.create_all(app=app)
 
         with app.app_context():
+            user = User.query.filter_by(username="bob").first()
+            if user :
+                db.session.delete(user)
+                db.session.commit()
             new_user = User(username="bob", password=generate_password_hash("testpassword123456", method='sha256'),
                                 weight="70", height="180")
             db.session.add(new_user)
@@ -20,6 +28,8 @@ class TestDatabase(unittest.TestCase):
     
     def test_delete_user(self):
         db.init_app(app)
+        if not path.exists('fitness_database.db'):
+            db.create_all(app=app)
 
         with app.app_context():
             user = User.query.filter_by(username="bob").first()
